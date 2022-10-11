f8092419bc967a077d86cd633a0e7e92fe875978
Christopher Leggett <chris@leggett.dev>
2022-10-11 09:10:47 -0400

    Initial commit, super basic shell implementation.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::{io::{stdin, stdout, Write}, process::Command};
+
+fn main() {
+    loop {
+        print!("> ");
+        stdout().flush();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        let mut child = Command::new(command)
+            .args(args)
+            .spawn()
+            .unwrap();
+
+        child.wait();
+    }
+}
