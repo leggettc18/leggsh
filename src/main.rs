589cffb9069180e4f17ff0040aee559b9fed456c
Christopher Leggett <chris@leggett.dev>
2022-10-11 09:15:54 -0400

    Implement cd

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{stdin, stdout, Write}, process::Command};
+use std::{io::{stdin, stdout, Write}, process::Command, path::Path, env};
 
 fn main() {
     loop {
@@ -12,11 +12,22 @@ fn main() {
         let command = parts.next().unwrap();
         let args = parts;
 
-        let mut child = Command::new(command)
-            .args(args)
-            .spawn()
-            .unwrap();
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            },
+            command => {
+                let mut child = Command::new(command)
+                    .args(args)
+                    .spawn()
+                    .unwrap();
 
-        child.wait();
+                child.wait();
+            }
+        }
     }
 }
